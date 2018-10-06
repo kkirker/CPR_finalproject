8c53584db53ca36b21812dc4398d6d9825cd6523
Kamon <Kamon.kirker@gmail.com>
2018-10-05 22:13:38 -0700

    1 component 2 component 3 component 4!

---
+++ client/src/components/landingComponentsExtra/UnderHeroImage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'react-emotion'
+
+const UnderHeroImageStyle = styled('div') ({
+
+})
+
+
+const UnderHeroImage = () => {
+  return (
+    <UnderHeroImageStyle>
+        <img src='https://www.lafd.org/sites/default/files/hands-only-cpr-steps.png'/>
+    </UnderHeroImageStyle>
+  )
+}
+
+export default UnderHeroImage
