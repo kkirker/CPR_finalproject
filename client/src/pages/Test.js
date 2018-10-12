283ce64f6583573bbfd583f8f7f4fd43d4160b35
Kamon <Kamon.kirker@gmail.com>
2018-10-11 19:24:57 -0700

    test json is available for testing

--- client/src/pages/Test.js
+++ client/src/pages/Test.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import styled from 'react-emotion'
 import TestContainer from '../components/testComponents/TestContainer'
+import Question from '../components/testComponents/Question'
+import TestStartButton from '../components/testComponents/TestStartButton';
 
 
 const TestPageWrapper = styled('div') ({
@@ -16,8 +18,31 @@ class Test extends Component {
     
         this.state = {
           version: 1,
+          showQuestions: false,
         };
+        this.handleClick = this.handleClick.bind(this);
       }
+
+    componentDidUpdate() {
+        console.log(this.state);
+    }
+
+
+    handleClick = (e) => {
+        e.preventDefault();
+        this.setState(state => ({
+            showQuestions: true
+        }));
+        console.log(this.state.showQuestions);
+    }
+
+    displayQuestions = () => {
+        if(this.state.showQuestions) {
+            return(
+                <Question/>
+            )
+        }
+    }
 
 
     render() {
@@ -26,7 +51,11 @@ class Test extends Component {
 
         return(
             <TestPageWrapper>
-                <TestContainer/>
+                <TestStartButton onClick={this.handleClick}/>
+                <button >click</button>
+                <TestContainer handleClick={this.handleClick} displayQuestions={this.displayQuestions}>
+                
+                </TestContainer>
             </TestPageWrapper>
 
         );
