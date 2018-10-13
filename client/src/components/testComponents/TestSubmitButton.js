625a91ef24999427d128837b11ecba03919694d3
Kamon <Kamon.kirker@gmail.com>
2018-10-13 12:30:19 -0700

    test submit button in

---
+++ client/src/components/testComponents/TestSubmitButton.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import purple from '@material-ui/core/colors/purple';
+import blue from '@material-ui/core/colors/blue';
+import { Link } from 'react-router-dom'
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  margin: {
+    margin: theme.spacing.unit,
+  },
+  cssRoot: {
+    color: theme.palette.getContrastText(purple[500]),
+    backgroundColor: purple[500],
+    '&:hover': {
+      backgroundColor: purple[700],
+    },
+  },
+  bootstrapRoot: {
+    boxShadow: 'none',
+    textTransform: 'none',
+    fontSize: 16,
+    padding: '6px 12px',
+    border: '1px solid',
+    backgroundColor: '#007bff',
+    borderColor: '#007bff',
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:hover': {
+      backgroundColor: '#0069d9',
+      borderColor: '#0062cc',
+    },
+    '&:active': {
+      boxShadow: 'none',
+      backgroundColor: '#0062cc',
+      borderColor: '#005cbf',
+    },
+    '&:focus': {
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+    },
+  },
+});
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+  },
+});
+
+function TestSubmitButton(props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.container}>
+      <MuiThemeProvider theme={theme}>
+      <Link to='/results'>
+            <Button variant="contained" onClick={props.submitClick} color="primary" className={classes.margin}>
+            Submit Answers
+            </Button>
+        </Link>
+      </MuiThemeProvider>
+    </div>
+  );
+}
+
+TestSubmitButton.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(TestSubmitButton);
